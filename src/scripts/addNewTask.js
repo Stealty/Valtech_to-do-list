1e5e44a47d6cfc7d9240a6ac68204278e6f586ac
Gabriel Ribeiro <gabriel.ribeiro@valtech.com>
2022-05-27 11:29:46 -0300

    classNames changed to BEM. Wrap on big texts

--- src/scripts/addNewTask.js
+++ src/scripts/addNewTask.js
@@ -1,10 +1,10 @@
-const incompletedList = document.getElementById("taskList__list-incompleted");
-const completedList = document.getElementById("taskList__list-completed");
+const incompletedList = document.querySelector(".taskList__list--incompleted");
+const completedList = document.querySelector(".taskList__list--completed");
 
 const addTaskFormDesktop = document.querySelector(".addTask__Form--desktop");
 const addTaskInputDesktop = document.querySelector(".addTask__input--desktop");
 
-const addTaskFormModal = document.querySelector(".addTask_form--modal");
+const addTaskFormModal = document.querySelector(".addTask__form--modal");
 const addTaskInputModal = document.querySelector(".addTask__input--modal");
 
 let taskList = [];
@@ -53,9 +53,9 @@ function addNewTaskModal() {
     addTaskInputModal.value = "";
     addTaskInputDesktop.value = "";
     modal.style.display = "none";
-    modalButton.setAttribute("class", "open__modal-button-open");
-    modalButtonAxisX.setAttribute("class", "open__modal-input__line-x-open");
-    modalButtonAxisY.setAttribute("class", "open__modal-input__line-y-open");
+    modalButton.setAttribute("class", "modal__button--open");
+    modalButtonAxisX.setAttribute("class", "modal__inputLine__x--open");
+    modalButtonAxisY.setAttribute("class", "modal__inputLine__y--open");
     saveData();
     createNewTaskItem_incompleted(newTask);
     completedList.innerHTML = "";
@@ -86,30 +86,31 @@ function createNewTaskItem_incompleted({ taskName, id }) {
   let taskItem = document.createElement("li");
   let newInputCheckBox = document.createElement("input");
   let newItem = document.createElement("label");
-  newItem.setAttribute("for", "task__list__item-checkbox");
-  newItem.setAttribute("class", "task__list__item-label");
+  newItem.setAttribute("for", "taskList__item--checkbox");
+  newItem.setAttribute("class", "taskList__item--label");
   newItem.setAttribute("unselectable", "on");
   newItem.appendChild(document.createTextNode(taskName));
-  taskItem.setAttribute("class", "task__list__item-incompleted");
-  newInputCheckBox.setAttribute("class", "task__list__item-input");
-  newInputCheckBox.setAttribute("id", "task__list__item-checkbox");
+  taskItem.setAttribute("class", "taskList__item--incompleted");
+  newInputCheckBox.setAttribute("class", "taskList__item--input");
+  newInputCheckBox.setAttribute("id", "taskList__item--checkbox");
   newInputCheckBox.type = "checkbox";
   taskItem.addEventListener("click", () => validateTask(id));
   taskItem.appendChild(newInputCheckBox);
   taskItem.appendChild(newItem);
   incompletedList.appendChild(taskItem);
+  enable();
 }
 
 function createNewTaskItem_completed({ taskName, id }) {
   let taskItem = document.createElement("li");
   let newInputCheckBox = document.createElement("input");
   let newItem = document.createElement("label");
-  newItem.setAttribute("for", "task__list__item-checkbox");
-  newItem.setAttribute("class", "task__list__item-label");
+  newItem.setAttribute("for", "taskList__item--checkbox");
+  newItem.setAttribute("class", "taskList__item--label");
   newItem.appendChild(document.createTextNode(taskName));
-  taskItem.setAttribute("class", "task__list__item-completed");
-  newInputCheckBox.setAttribute("class", "task__list__item-input");
-  newInputCheckBox.setAttribute("id", "task__list__item-checkbox");
+  taskItem.setAttribute("class", "taskList__item--completed");
+  newInputCheckBox.setAttribute("class", "taskList__item--input");
+  newInputCheckBox.setAttribute("id", "taskList__item--checkbox");
   newInputCheckBox.type = "checkbox";
   newInputCheckBox.checked = true;
   taskItem.addEventListener("click", () => validateTask(id));
--- src/scripts/toggleModal.js
+++ src/scripts/toggleModal.js
@@ -1,42 +1,42 @@
-const modalButton = document.querySelector("#toggleModal-button");
+const modalButton = document.querySelector("#toggleModal--button");
 const modal = document.querySelector(".modal");
 
-const modalButtonAxisX = document.querySelector("#open__modal-input__line-x");
-const modalButtonAxisY = document.querySelector("#open__modal-input__line-y");
+const modalButtonAxisX = document.querySelector("#modal__inputLine--x");
+const modalButtonAxisY = document.querySelector("#modal__inputLine--y");
 
 window.addEventListener("load", () => {
   modal.style.display = "none";
-  modalButton.setAttribute("class", "open__modal-button-open");
+  modalButton.setAttribute("class", "modal__button--open");
 });
 
 function enable() {
-  document.querySelector(".my__tasks-body").style.overflowY = "scroll";
+  document.querySelector(".myTasks__body").style.overflowY = "scroll";
 }
 
 function disable() {
-  document.querySelector(".my__tasks-body").style.overflowY = "hidden";
+  document.querySelector(".myTasks__body").style.overflowY = "hidden";
 }
 
 toggleModal = () => {
   if (modal.style.display === "none") {
     modal.style.display = "block";
-    modalButton.setAttribute("class", "open__modal-button-close");
-    modalButtonAxisX.setAttribute("class", "open__modal-input__line-x-close");
-    modalButtonAxisY.setAttribute("class", "open__modal-input__line-y-close");
+    modalButton.setAttribute("class", "modal__button--close");
+    modalButtonAxisX.setAttribute("class", "modal__inputLine__x--close");
+    modalButtonAxisY.setAttribute("class", "modal__inputLine__y--close");
     disable();
   } else {
     modal.style.display = "none";
-    modalButton.setAttribute("class", "open__modal-button-open");
-    modalButtonAxisX.setAttribute("class", "open__modal-input__line-x-open");
-    modalButtonAxisY.setAttribute("class", "open__modal-input__line-y-open");
+    modalButton.setAttribute("class", "modal__button--open");
+    modalButtonAxisX.setAttribute("class", "modal__inputLine__x--open");
+    modalButtonAxisY.setAttribute("class", "modal__inputLine__y--open");
     enable();
   }
 };
 
 addEventListener("resize", () => {
   modal.style.display = "none";
-  modalButton.setAttribute("class", "open__modal-button-open");
-  modalButtonAxisX.setAttribute("class", "open__modal-input__line-x-open");
-  modalButtonAxisY.setAttribute("class", "open__modal-input__line-y-open");
+  modalButton.setAttribute("class", "modal__button--open");
+  modalButtonAxisX.setAttribute("class", "modal__inputLine__x--open");
+  modalButtonAxisY.setAttribute("class", "modal__inputLine__y--open");
   enable();
 });
